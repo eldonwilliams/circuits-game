048fd404a942cdd1a841ca4e1cf694a210f7007a
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-22 18:26:16 -0500

    share

--- Assets/Scripts/PokeHandler.cs
+++ Assets/Scripts/PokeHandler.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 
 public class PokeHandler : MonoBehaviour
 {
-    public Tile groundTile;
+    public GameObject groundTilePrefab;
 
     private Tilemap tilemap;
     private PlayerInput playerInput;
@@ -18,8 +18,26 @@ public class PokeHandler : MonoBehaviour
 
         Vector3Int pos = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         pos = new Vector3Int(pos.x, pos.y);
-        if (!tilemap.HasTile(pos))
-            tilemap.SetTile(pos, groundTile);
+        if (!tilemap.HasTile(pos)) {
+            Tile newTile = ScriptableObject.CreateInstance<Tile>();
+            newTile.gameObject = groundTilePrefab;
+            tilemap.SetTile(pos, newTile);
+        }
+
+        GameObject go_tile = tilemap.GetInstantiatedObject(pos);
+        go_tile.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        go_tile.transform.LeanScale(new Vector3(1,1,1), 0.35f).setEaseOutBack().setOnComplete(() => {
+            go_tile.transform.localScale = new Vector3(1f, 1f, 1f);
+        });
+
+        go_tile.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.0f);
+        LeanTween.alpha(go_tile, 1, 0.35f).setEaseOutBack();
+
+        Vector3 normalPosition = go_tile.transform.position;
+        go_tile.transform.position -= new Vector3(0, 0.75f);
+        go_tile.transform.LeanMove(tilemap.CellToWorld(pos), 0.35f).setEaseOutBack().setOnComplete(() => {
+            go_tile.transform.position = tilemap.CellToWorld(pos);
+        });
     }
 
     public void Start()
