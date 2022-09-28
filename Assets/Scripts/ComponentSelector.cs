9bef69f742a2a9bc09ee2780fa4224c2067823f3
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-27 20:43:27 -0700

    rem anim

--- Assets/Scripts/ComponentSelector.cs
+++ Assets/Scripts/ComponentSelector.cs
@@ -40,35 +40,35 @@ public class ComponentSelector : MonoBehaviour
         _numberText.text = _number < 0 ? "" : _number.ToString();
     }
 
-    public void Show(bool instant = false)
-    {
-        _backgroundImage.gameObject.LeanAlpha(1.0f, instant ? 0.0f : 0.5f).setDelay(instant ? 0.0f : 0.5f).setEaseOutBack();
-        _componentImage.gameObject.LeanAlpha(1.0f, instant ? 0.0f : 0.5f).setEaseOutBack();
-        _componentImage.gameObject.LeanScale(new Vector3(1.0f, 1.0f, 1.0f), instant ? 0.0f : 0.5f).setEaseOutBack();
-        _numberText.gameObject.LeanAlpha(1.0f, instant ? 0.0f : 0.5f).setEaseOutBack();
-        _numberText.gameObject.LeanScale(new Vector3(1.0f, 1.0f, 1.0f), instant ? 0.0f : 0.5f).setEaseOutBack();
-    }
-
-    public void Hide(bool instant = false)
-    {
-        _backgroundImage.gameObject.LeanAlpha(0.0f, instant ? 0.0f : 0.5f).setDelay(instant ? 0.0f : 0.5f).setEaseInBack();
-        _componentImage.gameObject.LeanAlpha(0.0f, instant ? 0.0f : 0.5f).setEaseInBack();
-        _componentImage.gameObject.LeanScale(new Vector3(0.9f, 0.9f, 1.0f), instant ? 0.0f : 0.5f).setEaseInBack();
-        _numberText.gameObject.LeanAlpha(0.0f, instant ? 0.0f : 0.5f).setEaseInBack();
-        _numberText.gameObject.LeanScale(new Vector3(0.5f, 0.5f, 1.0f), instant ? 0.0f : 0.5f).setEaseInBack();
-        LeanTween.alp
-    }
-
-    IEnumerator IntroCoroutine()
-    {
-        Hide(true);
-        yield return new WaitForSeconds(1.0f);
-        Show();
-        yield return new WaitForSeconds(5.0f);
-        Hide();
-        yield return new WaitForSeconds(5.0f);
-        Show();
-    }
+    // public void Show(bool instant = false)
+    // {
+    //     _backgroundImage.gameObject.LeanAlpha(1.0f, instant ? 0.0f : 0.5f).setDelay(instant ? 0.0f : 0.5f).setEaseOutBack();
+    //     _componentImage.gameObject.LeanAlpha(1.0f, instant ? 0.0f : 0.5f).setEaseOutBack();
+    //     _componentImage.gameObject.LeanScale(new Vector3(1.0f, 1.0f, 1.0f), instant ? 0.0f : 0.5f).setEaseOutBack();
+    //     _numberText.gameObject.LeanAlpha(1.0f, instant ? 0.0f : 0.5f).setEaseOutBack();
+    //     _numberText.gameObject.LeanScale(new Vector3(1.0f, 1.0f, 1.0f), instant ? 0.0f : 0.5f).setEaseOutBack();
+    // }
+    //
+    // public void Hide(bool instant = false)
+    // {
+    //     _backgroundImage.gameObject.LeanAlpha(0.0f, instant ? 0.0f : 0.5f).setDelay(instant ? 0.0f : 0.5f).setEaseInBack();
+    //     _componentImage.gameObject.LeanAlpha(0.0f, instant ? 0.0f : 0.5f).setEaseInBack();
+    //     _componentImage.gameObject.LeanScale(new Vector3(0.9f, 0.9f, 1.0f), instant ? 0.0f : 0.5f).setEaseInBack();
+    //     _numberText.gameObject.LeanAlpha(0.0f, instant ? 0.0f : 0.5f).setEaseInBack();
+    //     _numberText.gameObject.LeanScale(new Vector3(0.5f, 0.5f, 1.0f), instant ? 0.0f : 0.5f).setEaseInBack();
+    //     LeanTween.alp
+    // }
+    //
+    // IEnumerator IntroCoroutine()
+    // {
+    //     Hide(true);
+    //     yield return new WaitForSeconds(1.0f);
+    //     Show();
+    //     yield return new WaitForSeconds(5.0f);
+    //     Hide();
+    //     yield return new WaitForSeconds(5.0f);
+    //     Show();
+    // }
     
     public void OnEnable()
     {
@@ -76,7 +76,7 @@ public class ComponentSelector : MonoBehaviour
         _componentImage = Global.GetGameObjectOfNameInChildren(gameObject, "Image").GetComponent<Image>();
         _numberText = gameObject.GetComponentInChildren<TMP_Text>();
         
-        Hide(true);
-        StartCoroutine(IntroCoroutine());
+        // Hide(true);
+        // StartCoroutine(IntroCoroutine());
     }
 }
