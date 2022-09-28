3df31498683e987775e62d7a649409dc960cacdf
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-28 02:31:44 -0500

    Update Wire.cs

--- Assets/Scripts/Components/Wire.cs
+++ Assets/Scripts/Components/Wire.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class Wire : ComponentBase
 {
     public override string Name { get => "Wire"; }
     public override Sprite Image { get => Resources.Load<Sprite>("Components/wire-image"); }
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+        tileData.sprite = Resources.Load<Sprite>("Components/Wire/NS");
+    }
 }
