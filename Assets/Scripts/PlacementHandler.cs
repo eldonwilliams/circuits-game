e4d5d7740b8b25314e41ae26bd4e3914f48bcf55
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-28 02:31:41 -0500

    Update PlacementHandler.cs

--- Assets/Scripts/PlacementHandler.cs
+++ Assets/Scripts/PlacementHandler.cs
@@ -4,19 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class PlacementHandler : MonoBehaviour
 {
     public GameObject componentSelectionPrefab;
     public GameObject componentSelectionDisplay;
+    public Tilemap componentTilemap;
     public ComponentBase SelectedComponent
     { get => _selectedComponent; }
+
+    public delegate void OnComponentChangeDelegate(ComponentBase newComponent);
+
+    public OnComponentChangeDelegate OnComponentChange;
 
     private ComponentBase _selectedComponent;
 
     public void ChangeComponent(ComponentBase to)
     {
         _selectedComponent = to;
+        OnComponentChange(to);
     }
 
     public void DisplayComponentSelections()
@@ -30,7 +38,7 @@ public class PlacementHandler : MonoBehaviour
         {
             var display = Instantiate(componentSelectionPrefab, componentSelectionDisplay.transform);
             var displaySelector = display.GetComponent<ComponentSelector>();
-            displaySelector.Component = (ComponentBase) Activator.CreateInstance(component);
+            displaySelector.Component = (ComponentBase) ScriptableObject.CreateInstance(component);
             displaySelector.Number = -1;
         }
     }
@@ -38,5 +46,30 @@ public class PlacementHandler : MonoBehaviour
     public void Start()
     {
         DisplayComponentSelections();
+        ChangeComponent(ScriptableObject.CreateInstance<Dragger>());
+
+        var placementHandler = Global.GetFirstOccurenceOfInRootScene<PlayerInput>();
+        var placing = false;
+        placementHandler.onActionTriggered += delegate(InputAction.CallbackContext context)
+        {
+            switch (context.action.name)
+            {
+                case "PlaceStart":
+                    if (_selectedComponent.Name == "Dragger") return;
+                    placing = context.ReadValue<float>() != 0.0f;
+                    break;
+                case "PlaceMove":
+                    if (placing == false) return;
+                    
+                    Vector3Int pos = componentTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                    pos = new Vector3Int(pos.x, pos.y);
+                    if (_selectedComponent.CanPlace(pos) && ((ComponentBase)componentTilemap.GetTile(pos))?.Name != _selectedComponent.Name)
+                    {
+                        componentTilemap.SetTile(pos, (TileBase) ScriptableObject.CreateInstance(_selectedComponent.GetType()));
+                    }
+                    
+                    break;
+            }
+        };
     }
 }
