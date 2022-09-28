94ab32d83ca078913aefc791e7831b9977c1fc5f
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-28 02:31:47 -0500

    Update ComponentBase.cs

--- Assets/Scripts/ComponentBase.cs
+++ Assets/Scripts/ComponentBase.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 /**
  * Describes the basic parts of a Component
  * Can be extended using a sub-class
  */
-public class ComponentBase
+public class ComponentBase : Tile
 {
     public virtual string Name { get => "COMP_BASE"; }
     /** The Sprite to use when previewing this Component */
     public virtual Sprite Image { get => Resources.Load<Sprite>("Components/cube"); }
-    
+
     public virtual bool CanPlace(Vector3Int at)
     {
-        var renderers = Global.GetFirstOccurenceOfInScene<Grid>().GetComponentsInChildren<TilemapRenderer>().Where(renderer => renderer.sortingOrder == 1);
+        var renderers = Global.GetFirstOccurenceOfInRootScene<Grid>().GetComponentsInChildren<TilemapRenderer>().Where(renderer => renderer.sortingOrder == 0);
         Tilemap tilemap = default;
         foreach (var renderer in renderers)
         {
             tilemap = renderer.GetComponent<Tilemap>();
         }
-
+        
         return tilemap ? tilemap.HasTile(at) : false;
     }
 }
